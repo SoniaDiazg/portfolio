a0a3a9b772ab1fa49793560523a2595d58c5b823
Sonia <soniadiazgarzonsdg@gmail.com>
2023-02-02 21:43:46 +0100

    terminando resposive tablet

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,3 +1,8 @@
+<?php 
+	$year = date("Y");
+?>
+
+
 <nav class="navDesktop">
 		<button id="button-menu" class="button-menu"><a href="index.php"><img class="letraS" src="assets/rsc/img/s.svg" alt="letra s"></a></button>
 		<br><br>
@@ -12,5 +17,8 @@
 			<a href="https://www.linkedin.com/in/sonia-díaz-garzon" target="_blank"><img src="assets/rsc/img/icono_linkedin.svg" alt="icono Linkedin"></a>
 			<a href="mailto:soniadiazgarzonsdg@gmail.com" target="_blank"><img src="assets/rsc/img/icono_email.svg" alt="icono Email"></a>
 			<a href="https://github.com/SoniaDiazg" target="_blank"><img src="assets/rsc/img/icono_gitHub.svg" alt="icono gitHub"></a>
+		</div>
+		<div id="copyright">
+			<p> &copy; Sonia Diaz Garzon <?php echo $year; ?> </p>
 		</div>
 	</nav>
